53c440a5ac6c7d07255ec19b6f5f88ab3c4e1cf7
Ruben Harutyunyan <harutyunyan.ruben@gmail.com>
2016-12-18 17:19:46 +0400

    Added lab 5.

---
+++ Euler-5/main.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+
+import math
+import matplotlib.pyplot as plt
+# Տվյալներ
+a, b = 0, 1
+h = 0.1
+n = (b - a) / h
+yzero = 0
+
+
+# f(x,y)
+def dy(x, y):
+    return x * math.pow(y, 2) + 1
+
+
+# Էյլերի մեթոդ
+def euler():
+    x = a
+    y = yzero
+    points = [(x, y)]
+    for i in range(1, int(n) + 1):
+        x = a + i * h
+        y += h * dy(x, y)
+        points.append((x, y))
+    return points
+
+
+# Աղյուսակի արտատպում
+def printtable(pts):
+    for i in range(len(pts)):
+        print("t={:.0f} X={:.1f} Y={:10.5f}".format(i, pts[i][0], pts[i][1]))
+
+# Գրաֆիկի գծում
+
+
+def plotgraph(pts):
+    x = []
+    y = []
+    for pt in pts:
+        x.append(pt[0])
+        y.append(pt[1])
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.title("Euler")
+    plt.plot(x, y, linewidth=1, label='Euler')
+    plt.legend()
+    plt.show()
+
+# Սկիզբ
+
+
+def main():
+    pts = euler()
+    printtable(pts)
+    plotgraph(pts)
+
+if __name__ == "__main__":
+    main()
